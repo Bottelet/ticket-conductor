b95f9ea17107f6c998e134853922bd45cf993360
Casper Bottelet <cb@obviux.dk>
2017-08-20 16:01:19 +0200

    fillout the controller

--- ticket-conductor-backend/app/Http/Controllers/CommentsController.php
+++ ticket-conductor-backend/app/Http/Controllers/CommentsController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Comment;
 
 class CommentsController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+      Comment::create($request->all());
+    }
 }
--- ticket-conductor-backend/app/Models/Comment.php
+++ ticket-conductor-backend/app/Models/Comment.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'text'
+    ];
+
     public function ticket()
     {
         return $this->belongsTo(Ticket::class);
