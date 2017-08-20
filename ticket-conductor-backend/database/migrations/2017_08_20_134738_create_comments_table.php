db56b5f4da9c96f6a8a8b5e04546190e8ca6130e
Casper Bottelet <cb@obviux.dk>
2017-08-20 15:53:10 +0200

    create migration for comments

---
+++ ticket-conductor-backend/database/migrations/2017_08_20_134738_create_comments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('text');
+            $table->integer('ticket_id')->references('id')->on('tickets')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
